f83a85b29156a9933ee89dc422824bbe243de7d0
Ed Goold <teddy_goold@hotmail.com>
2017-12-14 19:59:30 +0000

    minor amend1

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -21,14 +22,17 @@ def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
+
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
+
 #nothing happens until we call the methods
 students = input_students
 print_header
