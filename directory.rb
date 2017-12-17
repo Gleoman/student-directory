68088d334161fa69bfe55cdb3fa5ad01a9da7398
Ed Goold <teddy_goold@hotmail.com>
2017-12-17 17:18:32 +0000

    directory

--- directory.rb
+++ directory.rb
@@ -1,7 +1,6 @@
 @students = [] # an empty array accessible to all methods
 
 def interactive_menu
-  @students = []
   loop do
     print_menu
     process(gets.chomp)
@@ -11,6 +10,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" #9 because we'll be adding more items
 end
 
@@ -26,6 +26,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -106,6 +108,18 @@ end
 
 def print_footer
     puts "Overall, we have #{@students.count} great students".center(5)
+end
+
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+      @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+      end
+    file.close
 end
 #nothing happens until we call the methods
 interactive_menu
